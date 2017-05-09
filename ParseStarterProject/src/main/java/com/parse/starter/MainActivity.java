ec280f95133eb1a0e9d09904c7d9b79df76a0161
Maxim Aslamov <motokorochi@openmailbox.org>
2017-05-09 19:59:10 +0300

    Theme changing part 1

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -14,22 +14,29 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Fragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import com.parse.ParseAnalytics;
 
 
 public class MainActivity extends ActionBarActivity {
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) 
+  protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    ProgressBar progressbar = new ProgressBar(this);
+    progressBar.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+                  LayoutParams.WRAP_CONTENT, Garavity.CENTER));
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
 
   @Override
-  public boolean onCreateOptionsMenu(Menu menu) 
+  public boolean onCreateOptionsMenu(Menu menu)
   {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -37,12 +44,36 @@ public class MainActivity extends ActionBarActivity {
   }
 
   @Override
-  public boolean onOptionsItemSelected(MenuItem item) 
+  public boolean onOptionsItemSelected(MenuItem item)
   {
     // Handle action bar item clicks here. The action bar will
     // automatically handle clicks on the Home/Up button, so long
     // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
+    switch(item.getItemId())
+    {
+      case android.R.id.home:
+          finish();
+          break;
+
+      case android.R.id.activity_constructor:
+          startActivity(new Intent(ConstructorPage.This, ConstructorPage.class));
+          break;
+
+      case android.R.id.activity_favourite:
+        startActivity(new Intent(FavouriteCitations.This, FavouriteCitations.class));
+        break;
+
+      case android.R.id.activity_search:
+        startActivity(new Intent(SearchPage.This, SearchPage.class));  
+        break;
+
+      case android.R.id.activity_:
+        startActivity(new Intent(FavouriteCitations.This, FavouriteCitations.class));
+        break;
+
+
+    }
+
 
     //noinspection SimplifiableIfStatement
     if (id == R.id.action_settings) {
@@ -53,14 +84,14 @@ public class MainActivity extends ActionBarActivity {
   }
 
   public static final String SOCIAL_NETWORK_TAG = "SocialIntegrationMain.SOCIAL_NETWORK_TAG";
-    
+
   @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data)  
+  protected void onActivityResult(int requestCode, int resultCode, Intent data)
   {
     super.onActivityResult(requestCode, resultCode, data);
 
     Fragment fragment = getSupportFragmentManager().findFragmentByTag(SOCIAL_NETWORK_TAG);
-    if (fragment != null) 
+    if (fragment != null)
     {
         fragment.onActivityResult(requestCode, resultCode, data);
     }
---
+++ ParseStarterProject/src/main/java/com/parse/starter/SettingsFragment.java
@@ -0,0 +1,59 @@
+package pakuteam.bedlam_experiment_0_1;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.SwitchPreference;
+import android.support.v7,preference.SharedPreference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.annotation.NonNull;
+
+public class SettingsFragment extends PreferenceFragmentCompat
+{
+ 	protected ListPreference lp;
+	protected SwitchPreference sp;
+
+	@Override
+	public void OnCreatePreferences(Bundle savedInstance, String s)
+	{
+		 	super.onCreate(savedInstance);
+			addPreferencesFromResource(R.xml.app_preferences);
+	}
+
+	@Override
+	public void onResume()
+	{
+			super.onResume();
+
+			getPreferenceManager().getSharedPreference().registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onPause()
+	{
+			super.onPause();
+
+			getPreferenceManager().getSharedPreference().unregisterOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+	{
+			String currValue = null;
+			lp = (ListPreference) findPreference("choose_theme");
+			sp = (SwitchPreference) findPreference("allow_sync");
+			if(@NonNull lp)
+			{
+					CharSequence currText = lp.getEntry();
+					String currValue = lp.getValue();
+			}
+
+			if(@NonNull currValue)
+			{
+
+			}
+
+	}
+
+}
